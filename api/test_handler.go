b15658c43ce65698c94e2d26a5f8e74d029e5814
wh1plash <whiplash2486@gmail.com>
2025-05-19 17:36:21 +0300

    rm containers

--- api/test_handler.go
+++ api/test_handler.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type TestHandler struct{}
 
@@ -9,5 +13,6 @@ func NewTestHandler() *TestHandler {
 }
 
 func (h TestHandler) HandleHealthy(c *fiber.Ctx) error {
-	return c.JSON("result", "ok")
+	fmt.Println("healthy check")
+	return c.JSON(fiber.Map{"result": "ok"})
 }
