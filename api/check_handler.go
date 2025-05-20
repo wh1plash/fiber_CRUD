36110da946a566fcc7827f60c7ef839c56f974fc
wh1plash <whiplash2486@gmail.com>
2025-05-20 13:48:54 +0300

    [200~

--- api/check_handler.go
+++ api/check_handler.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,6 +13,6 @@ func NewTestHandler() *TestHandler {
 }
 
 func (h TestHandler) HandleHealthy(c *fiber.Ctx) error {
-	// fmt.Println("check")
+	fmt.Println("check")
 	return c.JSON(fiber.Map{"result": "ok"})
 }
